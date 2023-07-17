ab1c473bd414290409f6e813bcb13c08cd3b4173
Pravin <pravincoding49@gmail.com>
2023-07-17 23:03:52 +0530

    Minor changes related to word-highlight and Section linear gradient

--- js/wordHighlight.js
+++ js/wordHighlight.js
@@ -1,20 +1,26 @@
-'use strict'
+'use strict';
 
-document.addEventListener('DOMContentLoaded', () =>{
-    const wordHighlightEls = document.querySelectorAll('.word_highlight')
+document.addEventListener('DOMContentLoaded', () => {
+    const wordHighlightEls = document.querySelectorAll('.word_highlight');
 
     const callback = (entries, observer) => {
         const [entry] = entries;
-        if(!entry.isIntersecting) return;
-        entry.target.classList.add('fade_gradient')
-    }
+
+        // if(!entry.isIntersecting) return;
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade_gradient');
+        } else {
+            entry.target.classList.remove('fade_gradient');
+        }
+    };
 
     const options = {
         root: null,
         threshold: 1,
-        rootMargin : '-150px'
+        rootMargin: '-50px 0px -150px 0px',
     };
 
     const observer = new IntersectionObserver(callback, options);
-    wordHighlightEls.forEach((word, index) => index !== 0 &&  observer.observe(word))
-})
+    wordHighlightEls.forEach((word, index) => index !== 0 && observer.observe(word));
+});
